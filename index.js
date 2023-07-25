be15a445425122ccedd54b4dbd0a41d72870ee23
gauravraj08 <136689227+gauravraj08@users.noreply.github.com>
2023-07-25 22:04:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var randomNumber1=Math.floor(Math.random()*6)+1;
 
-var dice="images/dice"+randomNumber1+".png";
+var dice="dice"+randomNumber1+".png";
 
 var imageSource=document.querySelectorAll("img")[0];
 
@@ -8,7 +8,7 @@ imageSource.setAttribute("src",dice);
 
 var randomNumber2=Math.floor(Math.random()*6)+1;
 
-var dice2="images/dice"+randomNumber2+".png";
+var dice2="dice"+randomNumber2+".png";
 
 document.querySelectorAll("img")[1].setAttribute("src",dice2);
 
